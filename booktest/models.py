d84559a2f5fe08198c88e4cc4869e9adfc8341c2
siredrun <1499402309@qq.com>
2018-06-17 18:25:55 +0800

    222222

--- booktest/models.py
+++ booktest/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField()
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=10)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=1000)
+    hbook = models.ForeignKey(BookInfo)
