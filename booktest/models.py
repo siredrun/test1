7aa8ef7a7bd28c42177ec70c6ab2c41b4b3e10af
siredrun <1499402309@qq.com>
2018-06-17 18:50:16 +0800

    21

--- booktest/models.py
+++ booktest/models.py
@@ -4,8 +4,22 @@ from django.db import models
 class BookInfo(models.Model):
     btitle = models.CharField(max_length=20)
     bpub_date = models.DateTimeField()
+    def __str__(self):
+        return self.pk
+
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=10)
     hgender = models.BooleanField()
     hcontent = models.CharField(max_length=1000)
     hbook = models.ForeignKey(BookInfo)
+    def __str__(self):
+        return self.pk
+
+class Person():
+    objects = Manage()
+    def __str__(self):
+        return 'abc'
+class Manage():
+    pass
+p = Person()
+p.age = 18
